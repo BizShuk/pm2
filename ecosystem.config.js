0bef35786e2452dacf9e7cf9180906262d39a163
shuk.liu <biz.shuk@gmail.com>
2026-07-06 22:46:55 +0800

    refactor: reformat arguments array and remove redundant configuration properties in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,15 @@ module.exports = {
         {
             name: "agy-pm2-system",
             script: "/Users/shuk/.local/bin/agy",
-            args: ["--add-dir", "/Users/shuk/projects/tmp/pm2", "-p", "'run /system-planner for current workspace'"],
+            args: [
+                "--add-dir",
+                "/Users/shuk/projects/tmp/pm2",
+                "-p",
+                "'run /system-planner for current workspace'"
+            ],
             namespace: "planner",
-            cwd: "/Users/shuk/projects/tmp/pm2",
             instances: 1,
-            cron: "20 0-9 * * *",
+            cron: "20 0-9 * * *"
         },
         // cron_test (default)
         {
@@ -17,7 +21,7 @@ module.exports = {
             args: ["hello", "world"],
             namespace: "default",
             instances: 1,
-            cron: "* * * * *",
+            cron: "* * * * *"
         }
-    ],
+    ]
 };
